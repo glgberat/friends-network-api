b435e20abf71245061922da85f9f56547b2681ef
Abbush <glgberat@yahoo.com>
2022-04-23 00:44:23 -0400

    User model

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/] // Regex Validation for e-mail
+    },
+    thoughts:  [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thoughts'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Users'
+    }]
+    },
+    )
